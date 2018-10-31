1a6a7eba9ba95bad4b26a5de7dcb396705b7cafd
Farrukh Amir <34297140+FarrukhAmiir@users.noreply.github.com>
2018-10-31 14:06:35 +0500

    chnges

---
+++ Assets/Sound.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class Sound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+
+   
+
+    AudioSource audio;
+    // Use this for initialization
+    public GameObject buttonPrefab;
+    GameObject prefabInstance;
+        public Sprite UnPressed,Pressed;
+    public void OnMouseOver()
+    {
+        //OnClick();
+        ////do stuff
+    }
+    void Start () {
+        //CreateInstances();
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+    }
+    public void OnClick()
+    {
+        audio = transform.GetComponent<AudioSource>();
+        audio.enabled = true;
+        audio.Play();
+        
+    }
+   
+    public void CreateInstances()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            prefabInstance = Object.Instantiate(buttonPrefab);
+            prefabInstance.transform.parent = GameObject.Find("Whites").transform;
+            Button button = prefabInstance.GetComponent<Button>();
+            button.onClick.AddListener(() => { OnButtonClicked(i); });
+        }
+    }
+
+    public void OnButtonClicked(int buttonIndex)
+    {
+       
+        transform.GetComponent<Image>().sprite = UnPressed;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse enter");
+        transform.GetComponent<Image>().sprite = Pressed;
+        OnClick();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Mouse exit");
+        transform.GetComponent<Image>().sprite = UnPressed;
+
+    }
+
+}
