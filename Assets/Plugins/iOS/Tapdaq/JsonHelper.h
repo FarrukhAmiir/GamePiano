c851ec29caad8a746cc20c23226b7502869b1eae
Farrukh Amir <34297140+FarrukhAmiir@users.noreply.github.com>
2018-11-16 16:11:39 +0500

    remove

--- Assets/Plugins/iOS/Tapdaq/JsonHelper.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Tapdaq/Tapdaq.h>
-
-@interface JsonHelper : NSObject
-
-+ (NSString *) toJsonString:(NSDictionary *) dict;
-+ (NSDictionary *) fromJsonString:(NSString *) json;
-
-@end
